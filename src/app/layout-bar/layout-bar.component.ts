73d8867fd16b6ab9dac9e1e8e88a74e33dae5f08
Juan Garay <ing.juandgarayc@gmail.com>
2023-08-31 21:08:01 -0500

    feat(bar): create the layout and styles for the bar

---
+++ src/app/layout-bar/layout-bar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'layout-bar',
+  templateUrl: './layout-bar.component.html',
+  styleUrls: ['./layout-bar.component.scss'],
+})
+export class LayoutBarComponent {}
