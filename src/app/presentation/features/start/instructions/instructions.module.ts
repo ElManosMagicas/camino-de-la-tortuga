3e5a4633f31364b50294b1533c9b9bea39ae594e
Juan Garay <ing.juandgarayc@gmail.com>
2023-09-10 17:39:40 -0500

    feat(layout): add layout and styles for the instructions page

--- src/app/presentation/features/start/instructions/instructions.module.ts
+++ src/app/presentation/features/start/instructions/instructions.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { InstructionsRoutingModule } from './instructions-routing.module';
 import { InstructionsPage } from './instructions.page';
+import { SliderComponent } from './components/slider/slider.component';
 
 
 @NgModule({
   declarations: [
-    InstructionsPage
+    InstructionsPage,
+    SliderComponent
   ],
   imports: [
     CommonModule,
