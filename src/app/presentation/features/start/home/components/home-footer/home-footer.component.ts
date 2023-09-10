04b63370d4f6db986357dafdcd63f064a8b9dac9
Juan Garay <ing.juandgarayc@gmail.com>
2023-09-10 08:32:50 -0500

    feat(methods): create methods to redirect to each route

--- src/app/presentation/features/start/home/components/home-footer/home-footer.component.ts
+++ src/app/presentation/features/start/home/components/home-footer/home-footer.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { APP_ROUTES as ROUTES } from '@app/app.routes';
+import { UtilService } from '@app/services/util.service';
 
 @Component({
   selector: 'home-footer',
   templateUrl: './home-footer.component.html',
   styleUrls: ['./home-footer.component.scss'],
 })
-export class HomeFooterComponent {}
+export class HomeFooterComponent {
+  public ROUTES = ROUTES;
+
+  constructor(private _utilService: UtilService) {}
+
+  public onGoToReadBook() {}
+
+  public onGoToInstructions() {
+    this._utilService.navigateTo(this.ROUTES.INSTRUCTIONS);
+  }
+
+  public onGoToConfiguration() {
+    this._utilService.navigateTo(this.ROUTES.CONFIGURATION);
+  }
+
+  public onGoToCredits() {
+    this._utilService.navigateTo(this.ROUTES.CREDITS);
+  }
+}
