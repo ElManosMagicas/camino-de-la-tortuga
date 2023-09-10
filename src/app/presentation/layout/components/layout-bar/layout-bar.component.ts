eb5d6eb58af4d522ccaf6cc1b4f6d42656f7bed0
Juan Garay <ing.juandgarayc@gmail.com>
2023-09-09 20:54:21 -0500

    feat(hide): hide and show the controls

--- src/app/presentation/layout/components/layout-bar/layout-bar.component.ts
+++ src/app/presentation/layout/components/layout-bar/layout-bar.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'layout-bar',
   templateUrl: './layout-bar.component.html',
   styleUrls: ['./layout-bar.component.scss'],
 })
-export class LayoutBarComponent {}
+export class LayoutBarComponent implements OnInit {
+  public toggleControls: boolean = false;
+
+  ngOnInit(): void {}
+
+  public onToggleControls(): void {
+    this.toggleControls = !this.toggleControls;
+  }
+}
