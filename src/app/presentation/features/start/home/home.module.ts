695c815b590c8f53da1460965aced048593196ab
Juan Garay <ing.juandgarayc@gmail.com>
2023-09-09 21:46:30 -0500

    feat(home): initialize home layout

--- src/app/presentation/features/start/home/home.module.ts
+++ src/app/presentation/features/start/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePage } from './home.page';
+import { HomeFooterComponent } from './components/home-footer/home-footer.component';
 
 
 @NgModule({
   declarations: [
-    HomePage
+    HomePage,
+    HomeFooterComponent
   ],
   imports: [
     CommonModule,
