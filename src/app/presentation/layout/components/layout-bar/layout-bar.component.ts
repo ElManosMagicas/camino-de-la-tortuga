5cb72d4221ef4a436c54fa8603018dc60710aea2
Juan Garay <ing.juandgarayc@gmail.com>
2023-09-22 16:53:20 -0500

    fix(controls): adjust bar to show controls when mouse is over

--- src/app/presentation/layout/components/layout-bar/layout-bar.component.ts
+++ src/app/presentation/layout/components/layout-bar/layout-bar.component.ts
@@ -8,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 export class LayoutBarComponent implements OnInit {
   public toggleControls: boolean = false;
 
+  private _mouseOverControls: boolean = false;
+  private _hideControlsTimeout: any;
+
   ngOnInit(): void {}
+
+  private _hideAfterFourSeconds() {
+    this._hideControlsTimeout = setTimeout(() => {
+      if (!this._mouseOverControls) {
+        this.toggleControls = false;
+      }
+    }, 4000);
+  }
 
   public onToggleControls(): void {
     this.toggleControls = !this.toggleControls;
+    if (!this.toggleControls) {
+      clearTimeout(this._hideControlsTimeout);
+    }
+    this._hideAfterFourSeconds();
+  }
+
+  public onMouseEnterControls(): void {
+    this._mouseOverControls = true;
+  }
+
+  public onMouseLeaveControls(): void {
+    this._mouseOverControls = false;
+    this._hideAfterFourSeconds();
   }
 }
