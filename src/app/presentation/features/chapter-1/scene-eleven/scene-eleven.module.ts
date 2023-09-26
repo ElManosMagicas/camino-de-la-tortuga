94bd6b429c1b05c7afb7eb52360e139eee3fe37d
Juan Garay <ing.juandgarayc@gmail.com>
2023-09-25 22:12:00 -0500

    feat(layout): add assets for scene 11

--- src/app/presentation/features/chapter-1/scene-eleven/scene-eleven.module.ts
+++ src/app/presentation/features/chapter-1/scene-eleven/scene-eleven.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { SceneElevenRoutingModule } from './scene-eleven-routing.module';
 import { SceneElevenPage } from './scene-eleven.page';
-
+import { LayoutModule } from '@app/presentation/layout/layout.module';
 
 @NgModule({
-  declarations: [
-    SceneElevenPage
-  ],
-  imports: [
-    CommonModule,
-    SceneElevenRoutingModule
-  ]
+  declarations: [SceneElevenPage],
+  imports: [CommonModule, SceneElevenRoutingModule, LayoutModule],
 })
-export class SceneElevenModule { }
+export class SceneElevenModule {}
