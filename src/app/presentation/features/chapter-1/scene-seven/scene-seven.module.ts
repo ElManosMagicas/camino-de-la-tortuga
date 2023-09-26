3c03f439b4bc5c73d7e1adc3dd6aae6d975c0488
Juan Garay <ing.juandgarayc@gmail.com>
2023-09-25 21:08:37 -0500

    feat(layout): add assets for scene 7 and add more gif variables

--- src/app/presentation/features/chapter-1/scene-seven/scene-seven.module.ts
+++ src/app/presentation/features/chapter-1/scene-seven/scene-seven.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { SceneSevenRoutingModule } from './scene-seven-routing.module';
 import { SceneSevenPage } from './scene-seven.page';
-
+import { LayoutModule } from '@app/presentation/layout/layout.module';
 
 @NgModule({
-  declarations: [
-    SceneSevenPage
-  ],
-  imports: [
-    CommonModule,
-    SceneSevenRoutingModule
-  ]
+  declarations: [SceneSevenPage],
+  imports: [CommonModule, SceneSevenRoutingModule, LayoutModule],
 })
-export class SceneSevenModule { }
+export class SceneSevenModule {}
