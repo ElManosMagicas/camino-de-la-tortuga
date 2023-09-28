39cd9087f9771bc1e68f5d6ba2878fa20b82ca0b
Juan Garay <ing.juandgarayc@gmail.com>
2023-09-27 21:57:12 -0500

    feat(introduction): add layout and styles for the introduction in scene 1

--- src/app/presentation/features/chapter-1/scene-one/scene-one.page.ts
+++ src/app/presentation/features/chapter-1/scene-one/scene-one.page.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { APP_ROUTES as ROUTES } from '@app/app.routes';
+import { AppFacade } from '@app/facades/app.facade';
+import { UtilService } from '@app/services/util.service';
 
 @Component({
   selector: 'chapter-1-scene-one',
   templateUrl: './scene-one.page.html',
   styleUrls: ['./scene-one.page.scss'],
 })
-export class SceneOnePage {}
+export class SceneOnePage implements OnInit {
+  public turtleName: string = '';
+  public placeholderText: string = 'Ingresa el nombre aquí';
+  public isButtonDisabled: boolean = true;
+
+  constructor(
+    private _utilService: UtilService,
+    private _appFacade: AppFacade
+  ) {}
+
+  ngOnInit(): void {}
+
+  public onInputChange(): void {
+    this.isButtonDisabled = this.turtleName.trim() === '';
+  }
+
+  public onClearPlaceholder(): void {
+    this.turtleName = '';
+  }
+
+  public onSubmit(): void {
+    if (this.turtleName.trim() !== '') {
+      this._appFacade.setTurtleName(this.turtleName);
+      this._utilService.navigateTo(ROUTES.CHAPTER_1_SCENE_2);
+    }
+  }
+}
