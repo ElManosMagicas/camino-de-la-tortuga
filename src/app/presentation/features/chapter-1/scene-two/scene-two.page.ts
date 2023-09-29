7cc548b702654b34fcdb459bf3b43b029b2e3bcf
Juan Garay <ing.juandgarayc@gmail.com>
2023-09-29 09:01:41 -0500

    feat(audio): include narrator audio for scene 2

--- src/app/presentation/features/chapter-1/scene-two/scene-two.page.ts
+++ src/app/presentation/features/chapter-1/scene-two/scene-two.page.ts
@@ -1,19 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import { APP_CONSTANTS as CONST } from '@app/app.constants';
+import { APP_ROUTES as ROUTES } from '@app/app.routes';
 import { Chapter1Facade } from '@app/facades/chapter-1.facade';
 import { UtilService } from '@app/services/util.service';
-import { APP_ROUTES as ROUTES } from '@app/app.routes';
 
 @Component({
   selector: 'chapter-1-scene-two',
   templateUrl: './scene-two.page.html',
   styleUrls: ['./scene-two.page.scss'],
 })
-export class SceneTwoPage implements OnInit {
+export class SceneTwoPage implements OnInit, AfterViewInit {
+  @ViewChild('cap1Esc2Narrator') audioPlayer: ElementRef;
+
+  public CONST = CONST;
+
   constructor(
+    private _renderer: Renderer2,
     private _chapter1Facade: Chapter1Facade,
     private _utilService: UtilService
   ) {}
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.playAudio();
+    }, 5000);
+  }
+
+  public playAudio() {
+    const audioElement: HTMLAudioElement = this.audioPlayer.nativeElement;
+    if (audioElement.paused) {
+      audioElement.play();
+    }
+  }
 
   public onGoToNextPage(): void {
     this._chapter1Facade.goToNextStep();
