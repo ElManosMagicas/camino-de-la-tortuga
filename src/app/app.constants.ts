dcfd8327e13114755f55040476fd80acb2a0d61c
Juan Garay <ing.juandgarayc@gmail.com>
2023-09-29 08:37:34 -0500

    feat(constant): create constant file and store the audio urls

---
+++ src/app/app.constants.ts
@@ -0,0 +1,14 @@
+export const APP_CONSTANTS = {
+  AUDIO: {
+    NARRATOR_CHAPTER_1_SCENE_1: '/assets/audios/cap1-esc1-narrator.mp3',
+    NARRATOR_CHAPTER_1_SCENE_2: '/assets/audios/cap1-esc2-narrator.mp3',
+    NARRATOR_CHAPTER_1_SCENE_3: '/assets/audios/cap1-esc3-narrator.mp3',
+    NARRATOR_CHAPTER_1_SCENE_4: '/assets/audios/cap1-esc4-narrator.mp3',
+    CUY_CHAPTER_1_SCENE_5: '/assets/audios/cap1-esc5-cuy.mp3',
+    NARRATOR_CHAPTER_1_SCENE_6: '/assets/audios/cap1-esc6-narrator.mp3',
+    NARRATOR_CHAPTER_1_SCENE_7: '/assets/audios/cap1-esc7-narrator.mp3',
+    NARRATOR_CHAPTER_1_SCENE_9: '/assets/audios/cap1-esc9-narrator.mp3',
+    NARRATOR_CHAPTER_1_SCENE_10: '/assets/audios/cap1-esc10-narrator.mp3',
+    CUY_CHAPTER_1_SCENE_13: '/assets/audios/cap1-esc13-cuy.mp3',
+  },
+};
