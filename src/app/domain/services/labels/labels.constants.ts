f52ed5cad575e3071d4f3288f5418dffe4ee7e0a
Juan Garay <ing.juandgarayc@gmail.com>
2023-10-04 08:53:44 -0500

    fix(constants): update label constants for chapter 2

--- src/app/domain/services/labels/labels.constants.ts
+++ src/app/domain/services/labels/labels.constants.ts
@@ -1,5 +1,6 @@
 export const LABELS_CONSTANTS = {
   SCENES_LIST: {
+    //CHAPTER 1
     CHAPTER_1_SCENE_1: 'Humedal - Escena 1',
     CHAPTER_1_SCENE_2: 'Humedal - Escena 2',
     CHAPTER_1_SCENE_3: 'Humedal - Escena 3',
@@ -13,5 +14,17 @@ export const LABELS_CONSTANTS = {
     CHAPTER_1_SCENE_11: 'Humedal - Escena 11',
     CHAPTER_1_SCENE_12: 'Humedal - Escena 12',
     CHAPTER_1_SCENE_13: 'Humedal - Escena 13',
+    //CHAPTER 2
+    CHAPTER_2_SCENE_1: 'Bosque - Escena 1',
+    CHAPTER_2_SCENE_2: 'Bosque - Escena 2',
+    CHAPTER_2_SCENE_3: 'Bosque - Escena 3',
+    CHAPTER_2_SCENE_4: 'Bosque - Escena 4',
+    CHAPTER_2_SCENE_5: 'Bosque - Escena 5',
+    CHAPTER_2_SCENE_6: 'Bosque - Escena 6',
+    //CHAPTER 3
+    CHAPTER_3_SCENE_1: 'Desierto - Escena 1',
+    //CHAPTER 4
+    CHAPTER_4_SCENE_1: 'Llanos - Escena 1',
+    //CHAPTER 5
   },
 };
