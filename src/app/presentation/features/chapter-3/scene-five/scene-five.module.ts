d7e2b82bdbbad448b3a30f23de908f9b33208eee
Juan Garay <ing.juandgarayc@gmail.com>
2023-10-08 18:34:04 -0500

    feat(scene5): add assets, layout and styles for scene5

--- src/app/presentation/features/chapter-3/scene-five/scene-five.module.ts
+++ src/app/presentation/features/chapter-3/scene-five/scene-five.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { SceneFiveRoutingModule } from './scene-five-routing.module';
 import { SceneFivePage } from './scene-five.page';
-
+import { LayoutModule } from '@app/presentation/layout/layout.module';
 
 @NgModule({
-  declarations: [
-    SceneFivePage
-  ],
-  imports: [
-    CommonModule,
-    SceneFiveRoutingModule
-  ]
+  declarations: [SceneFivePage],
+  imports: [CommonModule, SceneFiveRoutingModule, LayoutModule],
 })
-export class SceneFiveModule { }
+export class SceneFiveModule {}
