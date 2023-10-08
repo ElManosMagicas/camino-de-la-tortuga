f28eb4bf979a436c5261513ee478867c3aec53e9
Juan Garay <ing.juandgarayc@gmail.com>
2023-10-08 18:33:18 -0500

    feat(scene2): add assets, layout and styles for scene2

--- src/app/presentation/features/chapter-3/scene-two/scene-two.page.ts
+++ src/app/presentation/features/chapter-3/scene-two/scene-two.page.ts
@@ -1,8 +1,74 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import { APP_CONSTANTS as CONST } from '@app/app.constants';
+import { APP_ROUTES as ROUTES } from '@app/app.routes';
+import { AppFacade } from '@app/facades/app.facade';
+import { Chapter3Facade } from '@app/facades/chapter-3.facade';
+import { UtilService } from '@app/services/util.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'chapter-3-scene-two',
   templateUrl: './scene-two.page.html',
   styleUrls: ['./scene-two.page.scss'],
 })
-export class SceneTwoPage {}
+export class SceneTwoPage implements OnInit, AfterViewInit {
+  @ViewChild('cap1Esc2Narrator') audioPlayer: ElementRef;
+  @ViewChild('cap3Esc1Snake') snakePlayer: ElementRef;
+
+  public CONST = CONST;
+  public currentRoute: string = '';
+
+  public turtleName$: Observable<string>;
+
+  constructor(
+    private _renderer: Renderer2,
+    private _appFacade: AppFacade,
+    private _chapter3Facade: Chapter3Facade,
+    private _utilService: UtilService
+  ) {}
+  ngOnInit(): void {
+    this.turtleName$ = this._appFacade.turtleName$;
+    this.currentRoute = this._utilService.getCurrentRoute();
+  }
+
+  ngAfterViewInit(): void {
+    // setTimeout(() => {
+    //   this.playAudio();
+    // }, 5000);
+  }
+
+  public playAudio() {
+    const audioElement: HTMLAudioElement = this.audioPlayer.nativeElement;
+    if (audioElement.paused) {
+      audioElement.play();
+    }
+  }
+
+  public onGoToNextPage(): void {
+    this._chapter3Facade.goToNextStep();
+    this._utilService.navigateTo(ROUTES.CHAPTER_3_SCENE_3);
+  }
+  public onGoToPreviousPage(): void {
+    this._chapter3Facade.goToPreviousStep();
+    this._utilService.navigateTo(ROUTES.CHAPTER_3_SCENE_1);
+  }
+  public onGoToConfiguration() {}
+  public onGoToBackpack() {}
+  public onRepeatScene() {
+    this._utilService.redirectToUrl(ROUTES.CHAPTER_3_SCENE_2);
+  }
+
+  public onPlaySnakeSound(): void {
+    // const snakeElement: HTMLAudioElement = this.snakePlayer.nativeElement;
+    // if (snakeElement.paused) {
+    //   snakeElement.play();
+    // }
+  }
+}
