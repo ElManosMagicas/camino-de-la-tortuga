84adb1b4bd291981f67f25f37c2fbd6d2f81235a
Juan Garay <ing.juandgarayc@gmail.com>
2023-10-08 18:31:21 -0500

    fix(routing): remove scene9, not necessary

--- src/app/presentation/features/chapter-3/scene-nine/scene-nine.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'chapter-3-scene-nine',
-  templateUrl: './scene-nine.page.html',
-  styleUrls: ['./scene-nine.page.scss'],
-})
-export class SceneNinePage {}
