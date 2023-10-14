4e833bf44baf7ad50915d9c42d475ffcf7978510
Juan Garay <ing.juandgarayc@gmail.com>
2023-10-14 09:52:41 -0500

    feat(subtitles): create constantsfor chapter 1,2,3,4,5

---
+++ src/app/presentation/features/chapter-2/chapter-2.subtitles.ts
@@ -0,0 +1,12 @@
+export const SUBTITLES_CHAPTER_2 = {
+  SCENE_ONE:
+    'Al entrar al bosque, la tortuga {turtleName} está espantada porque hay muchos árboles enormes y entre las ramas encuentra al tucán.',
+  SCENE_TWO:
+    '─ Señor tucán disculpe, ¿sabe si en este bosque viven las tortugas como yo?',
+  SCENE_THREE:
+    '─ Mmm… acá no he visto ninguna tortuga, solo conozco animales como la mirla, la ardilla o tucanes andinos como yo, pero podría saber más si me dices qué te gusta comer. A mí me encantan las frutas.',
+  SCENE_FOUR:
+    '─ Yo prefiero comer tallos y semillas. ¡Son deliciosos y llenan mi pancita!',
+  SCENE_FIVE:
+    '─ Ya veo, pues yo no como tallos ni semillas. No sé de dónde podrías venir. Será mejor que visites otro lugar.',
+};
---
+++ src/app/presentation/features/chapter-5/chapter-5.subtitles.ts
@@ -0,0 +1,8 @@
+export const SUBTITLES_CHAPTER_5 = {
+  SCENE_ONE:
+    'Al buscar otro amigo al cual preguntarle, la tortuga {turtleName} se encuentra en un nuevo lugar.  Aunque es caliente, no es lo suficiente para quemarse ni tan frío como para congelarse.',
+  SCENE_TWO:
+    'También hay unos nidos de tortuga cerca al río, rodeados con muchos tallos y semillas sabrosas. Y en la noche ningún animal la molestará.',
+  SCENE_THREE: 'Hay muchas tortugas como ella alrededor del río.',
+  SCENE_FIVE: '─¡Al fin encontré mi hogar!',
+};
