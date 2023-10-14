af892cd2621547ca02cf00ba001210a77fabe9a2
Juan Garay <ing.juandgarayc@gmail.com>
2023-10-14 15:49:20 -0500

    fix(module): import layout module

--- src/app/presentation/features/chapter-4/scene-two/scene-two.module.ts
+++ src/app/presentation/features/chapter-4/scene-two/scene-two.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { SceneTwoRoutingModule } from './scene-two-routing.module';
 import { SceneTwoPage } from './scene-two.page';
-
+import { LayoutModule } from '@app/presentation/layout/layout.module';
 
 @NgModule({
-  declarations: [
-    SceneTwoPage
-  ],
-  imports: [
-    CommonModule,
-    SceneTwoRoutingModule
-  ]
+  declarations: [SceneTwoPage],
+  imports: [CommonModule, SceneTwoRoutingModule, LayoutModule],
 })
-export class SceneTwoModule { }
+export class SceneTwoModule {}
