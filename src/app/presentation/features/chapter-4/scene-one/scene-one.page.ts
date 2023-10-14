47a54b731a23b9b67af775bbd83c8589c914ed3d
Juan Garay <ing.juandgarayc@gmail.com>
2023-10-14 17:48:50 -0500

    feat(scene-1): add layout and styles for scene 1

--- src/app/presentation/features/chapter-4/scene-one/scene-one.page.ts
+++ src/app/presentation/features/chapter-4/scene-one/scene-one.page.ts
@@ -1,8 +1,91 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import { APP_CONSTANTS as CONST } from '@app/app.constants';
+import { APP_ROUTES as ROUTES } from '@app/app.routes';
+import { AppFacade } from '@app/facades/app.facade';
+import { UtilService } from '@app/services/util.service';
+import { Observable, Subscription } from 'rxjs';
+import { SUBTITLES_CHAPTER_4 } from '../chapter-4.subtitles';
+import { Chapter4Facade } from '@app/facades/chapter-4.facade';
 
 @Component({
   selector: 'chapter-4-scene-one',
   templateUrl: './scene-one.page.html',
   styleUrls: ['./scene-one.page.scss'],
 })
-export class SceneOnePage {}
+export class SceneOnePage implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('cap4Esc1Narrator') audioPlayer: ElementRef;
+  @ViewChild('cap4Esc1Serpiente') tucanPlayer: ElementRef;
+
+  public CONST = CONST;
+  public currentRoute: string = '';
+  public turtleName: string;
+
+  public turtleName$: Observable<string>;
+
+  public turtleNameSubscription$: Subscription;
+
+  constructor(
+    private _renderer: Renderer2,
+    private _appFacade: AppFacade,
+    private _chapter4Facade: Chapter4Facade,
+    private _utilService: UtilService
+  ) {}
+
+  ngOnInit(): void {
+    this.turtleName$ = this._appFacade.turtleName$;
+    this.turtleNameSubscription$ = this.turtleName$.subscribe((name) => {
+      this.turtleName = name;
+    });
+    this.currentRoute = this._utilService.getCurrentRoute();
+  }
+
+  ngAfterViewInit(): void {
+    // setTimeout(() => {
+    //   this.playAudio();
+    // }, 5000);
+  }
+
+  ngOnDestroy(): void {
+    this.turtleNameSubscription$?.unsubscribe();
+  }
+
+  public getSubtitles(): string {
+    const name = this.turtleName || '';
+    return SUBTITLES_CHAPTER_4.SCENE_ONE.replace(/{turtleName}/g, name);
+  }
+
+  public playAudio() {
+    const audioElement: HTMLAudioElement = this.audioPlayer.nativeElement;
+    if (audioElement.paused) {
+      audioElement.play();
+    }
+  }
+
+  public onGoToNextPage(): void {
+    this._chapter4Facade.goToNextStep();
+    this._utilService.navigateTo(ROUTES.CHAPTER_4_SCENE_2);
+  }
+  public onGoToPreviousPage(): void {
+    this._utilService.navigateTo(ROUTES.MAP);
+  }
+  public onGoToConfiguration() {}
+  public onGoToBackpack() {}
+  public onRepeatScene() {
+    this._utilService.redirectToUrl(ROUTES.CHAPTER_4_SCENE_1);
+  }
+
+  public onPlayTucanSound(): void {
+    // const tucanElement: HTMLAudioElement = this.tucanPlayer.nativeElement;
+    // if (tucanElement.paused) {
+    //   tucanElement.play();
+    // }
+  }
+}
