fe9486a52638cf9b9f7e68946d090afa81805103
Juan Garay <ing.juandgarayc@gmail.com>
2023-10-16 20:25:27 -0500

    feat(success): include success for chapter 4

--- src/app/presentation/features/chapter-4/scene-six/scene-six.module.ts
+++ src/app/presentation/features/chapter-4/scene-six/scene-six.module.ts
@@ -4,9 +4,12 @@ import { CommonModule } from '@angular/common';
 import { SceneSixRoutingModule } from './scene-six-routing.module';
 import { SceneSixPage } from './scene-six.page';
 import { LayoutModule } from '@app/presentation/layout/layout.module';
+import { SuccessActivityChapterFourComponent } from '../components/success-activity-chapter-four/success-activity-chapter-four.component';
+
+const COMPONENTS = [SuccessActivityChapterFourComponent];
 
 @NgModule({
-  declarations: [SceneSixPage],
+  declarations: [SceneSixPage, ...COMPONENTS],
   imports: [CommonModule, SceneSixRoutingModule, LayoutModule],
 })
 export class SceneSixModule {}
