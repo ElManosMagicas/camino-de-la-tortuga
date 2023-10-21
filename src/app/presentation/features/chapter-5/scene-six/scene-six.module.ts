64fb2a0618ee3aad2f3eacb3346e8ed3bcd97086
Juan Garay <ing.juandgarayc@gmail.com>
2023-10-21 09:42:39 -0500

    feat(module): include the layout module

--- src/app/presentation/features/chapter-5/scene-six/scene-six.module.ts
+++ src/app/presentation/features/chapter-5/scene-six/scene-six.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { SceneSixRoutingModule } from './scene-six-routing.module';
 import { SceneSixPage } from './scene-six.page';
-
+import { LayoutModule } from '@app/presentation/layout/layout.module';
 
 @NgModule({
-  declarations: [
-    SceneSixPage
-  ],
-  imports: [
-    CommonModule,
-    SceneSixRoutingModule
-  ]
+  declarations: [SceneSixPage],
+  imports: [CommonModule, SceneSixRoutingModule, LayoutModule],
 })
-export class SceneSixModule { }
+export class SceneSixModule {}
