8d2c859fa550020edd00dd4f11e9d809ae0f0e15
Juan Garay <ing.juandgarayc@gmail.com>
2023-10-24 19:59:39 -0500

    feat(scene 4): add layout, styles and logic for activity

--- src/app/presentation/features/chapter-5/scene-four/scene-four.page.ts
+++ src/app/presentation/features/chapter-5/scene-four/scene-four.page.ts
@@ -1,8 +1,141 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  Renderer2,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
+import { APP_CONSTANTS as CONST } from '@app/app.constants';
+import { APP_ROUTES as ROUTES } from '@app/app.routes';
+import { ETURLTE_HOME } from '@app/core/enums/chapter-5-scene-4.enum';
+import { AppFacade } from '@app/facades/app.facade';
+import { UtilService } from '@app/services/util.service';
+import { Observable, Subscription } from 'rxjs';
+import { SUBTITLES_CHAPTER_5 } from '../chapter-5.subtitles';
+import { Chapter5Facade } from '@app/facades/chapter-5.facade';
+import { EPERFECT_DAY } from '@app/core/enums/chapter-4-scene-6.enum';
+import { IContextModal } from '@app/core/models/modal.model';
 
 @Component({
   selector: 'chapter-5-scene-four',
   templateUrl: './scene-four.page.html',
   styleUrls: ['./scene-four.page.scss'],
 })
-export class SceneFourPage {}
+export class SceneFourPage implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('cap5Esc1Narrator') audioPlayer: ElementRef;
+  @ViewChild('cap5Esc1Serpiente') tucanPlayer: ElementRef;
+
+  public CONST = CONST;
+  public EPERFECT_DAY = EPERFECT_DAY;
+  public currentRoute: string = '';
+  public turtleName: string;
+  public piecesClicked: boolean[] = [false, false, false];
+  public isDottedEnabled: boolean = false;
+  public dottedClicked: boolean[] = [false, false, false];
+  public piecesCompleted: boolean[] = [false, false, false];
+
+  public turtleName$: Observable<string>;
+  public turtleHome$: Observable<number>;
+
+  public turtleNameSubscription$: Subscription;
+  public turtleHomeSubscription$: Subscription;
+
+  constructor(
+    private _renderer: Renderer2,
+    private _appFacade: AppFacade,
+    private _chapter5Facade: Chapter5Facade,
+    private _utilService: UtilService
+  ) {}
+
+  ngOnInit(): void {
+    this.turtleName$ = this._appFacade.turtleName$;
+    this.turtleNameSubscription$ = this.turtleName$.subscribe((name) => {
+      this.turtleName = name;
+    });
+    this.turtleHome$ = this._chapter5Facade.turtleHome$;
+    this.turtleHomeSubscription$ = this.turtleHome$.subscribe((score) => {
+      score === ETURLTE_HOME.STATE_3 ? this.onContinue() : false;
+    });
+    this.currentRoute = this._utilService.getCurrentRoute();
+  }
+
+  ngAfterViewInit(): void {
+    // setTimeout(() => {
+    //   this.playAudio();
+    // }, 5000);
+  }
+
+  ngOnDestroy(): void {
+    this.turtleNameSubscription$?.unsubscribe();
+    this.turtleHomeSubscription$?.unsubscribe();
+  }
+
+  public getSubtitles(): string {
+    const name = this.turtleName || '';
+    return SUBTITLES_CHAPTER_5.SCENE_FIVE.replace(/{turtleName}/g, name);
+  }
+
+  public playAudio() {
+    const audioElement: HTMLAudioElement = this.audioPlayer.nativeElement;
+    if (audioElement.paused) {
+      audioElement.play();
+    }
+  }
+
+  public onGoToNextPage(): void {
+    this._chapter5Facade.goToNextStep();
+    this._utilService.navigateTo(ROUTES.CHAPTER_5_SCENE_5);
+  }
+  public onGoToPreviousPage(): void {
+    this._chapter5Facade.goToPreviousStep();
+    this._utilService.navigateTo(ROUTES.CHAPTER_5_SCENE_3);
+  }
+  public onGoToConfiguration() {}
+  public onGoToBackpack() {}
+  public onRepeatScene() {
+    this._utilService.redirectToUrl(ROUTES.CHAPTER_5_SCENE_4);
+  }
+
+  public onPlayTucanSound(): void {
+    // const tucanElement: HTMLAudioElement = this.tucanPlayer.nativeElement;
+    // if (tucanElement.paused) {
+    //   tucanElement.play();
+    // }
+  }
+
+  public onPieceClicked(index: number): void {
+    this.piecesClicked[index] = !this.piecesClicked[index];
+    this.updateDottedDisabled();
+    for (let i = 0; i < this.piecesClicked.length; i++) {
+      if (i !== index) {
+        this.piecesClicked[i] = false;
+      }
+    }
+  }
+
+  public updateDottedDisabled(): void {
+    this.isDottedEnabled = this.piecesClicked.some((clicked) => clicked);
+  }
+
+  public onDottedClicked(index: number): void {
+    this.dottedClicked[index] = !this.dottedClicked[index];
+    if (this.piecesClicked[index] && this.dottedClicked[index]) {
+      this._chapter5Facade.increaseTurtleHomeScore();
+      this.piecesCompleted[index] = true;
+    } else {
+      this.dottedClicked[index] = false;
+    }
+  }
+
+  public onContinue(): void {
+    this.piecesClicked = [false, false, false];
+    this.isDottedEnabled = false;
+    this.dottedClicked = [false, false, false];
+    this.piecesCompleted = [false, false, false];
+    this._chapter5Facade.resetTurtleHomeScore();
+    this._utilService.navigateTo(ROUTES.CHAPTER_5_SCENE_5);
+  }
+}
