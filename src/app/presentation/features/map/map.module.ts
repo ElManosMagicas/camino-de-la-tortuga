1979409f67826618bb393f6cc552b4335ac2acaa
Juan Garay <juan.garay@wearemo.com>
2023-10-26 07:58:21 -0500

    feat(map): add layout, cuy, textbox, chapter 1 and 2 styles

--- src/app/presentation/features/map/map.module.ts
+++ src/app/presentation/features/map/map.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { MapRoutingModule } from './map-routing.module';
 import { MapPage } from './map.page';
-
+import { LayoutModule } from '@app/presentation/layout/layout.module';
 
 @NgModule({
-  declarations: [
-    MapPage
-  ],
-  imports: [
-    CommonModule,
-    MapRoutingModule
-  ]
+  declarations: [MapPage],
+  imports: [CommonModule, MapRoutingModule, LayoutModule],
 })
-export class MapModule { }
+export class MapModule {}
--- src/app/presentation/features/map/map.page.ts
+++ src/app/presentation/features/map/map.page.ts
@@ -1,10 +1,90 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import { APP_CONSTANTS as CONST } from '@app/app.constants';
+import { APP_ROUTES as ROUTES } from '@app/app.routes';
+import { AppFacade } from '@app/facades/app.facade';
+import { UtilService } from '@app/services/util.service';
+import { Observable, Subscription } from 'rxjs';
+import { MapFacade } from '@app/facades/map.facade';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
-  styleUrls: ['./map.page.scss']
+  styleUrls: ['./map.page.scss'],
 })
-export class MapPage {
+export class MapPage implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('cap4Esc1Narrator') audioPlayer: ElementRef;
+  @ViewChild('cap4Esc1Serpiente') tucanPlayer: ElementRef;
 
+  public CONST = CONST;
+  public currentRoute: string = '';
+  public turtleName: string;
+  public showNextButton: boolean = false;
+  public showPreviousButton: boolean = false;
+
+  public turtleName$: Observable<string>;
+
+  public turtleNameSubscription$: Subscription;
+
+  constructor(
+    private _renderer: Renderer2,
+    private _appFacade: AppFacade,
+    private _utilService: UtilService
+  ) {}
+
+  ngOnInit(): void {
+    this.turtleName$ = this._appFacade.turtleName$;
+    this.turtleNameSubscription$ = this.turtleName$.subscribe((name) => {
+      this.turtleName = name;
+    });
+    this.currentRoute = this._utilService.getCurrentRoute();
+  }
+
+  ngAfterViewInit(): void {
+    // setTimeout(() => {
+    //   this.playAudio();
+    // }, 5000);
+  }
+
+  ngOnDestroy(): void {
+    this.turtleNameSubscription$?.unsubscribe();
+  }
+
+  // public getSubtitles(): string {
+  //   const name = this.turtleName || '';
+  //   return SUBTITLES_CHAPTER_4.SCENE_ONE.replace(/{turtleName}/g, name);
+  // }
+
+  public playAudio() {
+    const audioElement: HTMLAudioElement = this.audioPlayer.nativeElement;
+    if (audioElement.paused) {
+      audioElement.play();
+    }
+  }
+
+  public onGoToNextPage(): void {
+    this._utilService.navigateTo(ROUTES.CHAPTER_4_SCENE_2);
+  }
+  public onGoToPreviousPage(): void {
+    this._utilService.navigateTo(ROUTES.MAP);
+  }
+  public onGoToConfiguration() {}
+  public onGoToBackpack() {}
+  public onRepeatScene() {
+    this._utilService.redirectToUrl(ROUTES.MAP);
+  }
+
+  public onPlayTucanSound(): void {
+    // const tucanElement: HTMLAudioElement = this.tucanPlayer.nativeElement;
+    // if (tucanElement.paused) {
+    //   tucanElement.play();
+    // }
+  }
 }
