d8ff9496ee7dcc787ece56653892aa8c990308a9
Juan Garay <ing.juandgarayc@gmail.com>
2023-11-06 17:16:39 -0500

    fix(assets): remove hosting url from assets

--- src/app/presentation/features/start/instructions/constants/instructions-slider.constants.ts
+++ src/app/presentation/features/start/instructions/constants/instructions-slider.constants.ts
@@ -1,35 +1,35 @@
 export const INSTRUCTIONS_SLIDER_CONSTANTS = [
   {
-    icon: 'https://elcaminodelatortuga.com/assets/icons/btn-retroceder.svg',
+    icon: '/assets/icons/btn-retroceder.svg',
     alt: 'ïcono de botón retroceder',
     description:
       'Este es el botón <b>Atrás</b>. Con él puedes ir a la escena anterior.',
   },
   {
-    icon: 'https://elcaminodelatortuga.com/assets/icons/btn-avanzar.svg',
+    icon: '/assets/icons/btn-avanzar.svg',
     alt: 'ïcono de botón avanzar',
     description: `Este es el botón <b>Adelante</b>. Con él puedes ir a la escena siguiente.`,
   },
   {
-    icon: 'https://elcaminodelatortuga.com/assets/icons/btn-repetir-escena.svg',
+    icon: '/assets/icons/btn-repetir-escena.svg',
     alt: 'ïcono de botón repetir escena',
     description:
       'Este es el botón <b>Repetir</b>. Con él puedes volver a ver la escena.',
   },
   {
-    icon: 'https://elcaminodelatortuga.com/assets/icons/btn-mochila.svg',
+    icon: '/assets/icons/btn-mochila.svg',
     alt: 'ïcono de botón mochila',
     description:
       'Este es el botón <b>Mochila</b>. Con él puedes ver el equipaje de la tortuga y descubrir qué objetos le hacen falta.',
   },
   {
-    icon: 'https://elcaminodelatortuga.com/assets/icons/btn-configuracion.svg',
+    icon: '/assets/icons/btn-configuracion.svg',
     alt: 'ïcono de botón configuración',
     description:
       'Este es el botón <b>Configuración</b>. Con él puedes adecuar la experiencia como te resulte más cómoda.',
   },
   {
-    icon: 'https://elcaminodelatortuga.com/assets/icons/btn-configuracion.svg',
+    icon: '/assets/icons/btn-configuracion.svg',
     alt: 'ïcono de botón escena',
     description:
       'En este menú podrás encontrar cada uno de los capítulos del cuento y las escenas que hacen parte de cada capítulo para que navegues la historia.',
