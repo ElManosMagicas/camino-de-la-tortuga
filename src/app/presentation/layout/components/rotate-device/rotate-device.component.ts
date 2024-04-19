2af2734b6c5cdb32c7073a01633bdb5127aec82c
Juan Garay <ing.juandgarayc@gmail.com>
2024-04-19 08:04:53 -0500

    fix: modify component to adjust in modal

--- src/app/presentation/layout/components/rotate-device/rotate-device.component.ts
+++ src/app/presentation/layout/components/rotate-device/rotate-device.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'layout-rotate-device',
   templateUrl: './rotate-device.component.html',
   styleUrls: ['./rotate-device.component.scss'],
 })
-export class RotateDeviceComponent {}
+export class RotateDeviceComponent implements OnInit {
+  @Output() closeRotateDeviceEvent = new EventEmitter<void>();
+
+  ngOnInit(): void {}
+
+  public onClose(): void {
+    this.closeRotateDeviceEvent.emit();
+  }
+}
